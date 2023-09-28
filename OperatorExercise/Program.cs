09a33a821d2ffd298c8248d39ab1f669433c2d46
tommy santos <tommy1santos@yahoo.com>
2023-09-28 19:31:54 -0400

    Finished Operartors Exercise

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -1,51 +1,38 @@
-﻿namespace OperatorExercise
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace OperatorExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            using System;
+            // For division, create interger variables called a and b.
+            var a = 17;
+            var b = 4;
+            var quotient = a / b;
+            var remainder = a % b;
 
-class Program
-        {
-            static void Main()
+
+            Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}");
+
+            
+
+           
+
+            Console.WriteLine(AreaOfCircle());
+        }
+
+            // Define a method - which performs a functionality.
+            // that can be use over and over again.
+            public static double AreaOfCircle()
             {
-                // Exercise 1
-                int a = 17;
-                int b = 4;
-
-                // Addition
-                int sum = a + b;
-                Console.WriteLine($"Addition: {a} + {b} = {sum}");
-
-                // Subtraction
-                int difference = a - b;
-                Console.WriteLine($"Subtraction: {a} - {b} = {difference}");
-
-                // Multiplication
-                int product = a * b;
-                Console.WriteLine($"Multiplication: {a} * {b} = {product}");
-
-                // Division
-                int quotient = a / b;
-                Console.WriteLine($"Division (Quotient): {a} / {b} = {quotient}");
-
-                // Modulus
-                int remainder = a % b;
-                Console.WriteLine($"Modulus (Remainder): {a} % {b} = {remainder}");
-
-                If a = 17 and b = 4,
-                the quotient is: 4
-                the remainder is: 1
-                
-                //Exercise 2
-
-
-
+                Console.WriteLine("Please enter the radius of your circle:");
+                var radius = double.Parse(Console.ReadLine());
+                var area = Math.PI * Math.Pow(radius, 2);
+            
+                return area;
+        
             }
-        }
-
-        Console.WriteLine("Hello, World!");
-        }
+          
     }
 }
