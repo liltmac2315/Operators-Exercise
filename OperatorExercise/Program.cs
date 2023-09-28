f2c50faac3828641358fda9feed9bf8a90510430
tommy santos <tommy1santos@yahoo.com>
2023-09-28 13:17:07 -0400

    Method exercise done.

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,48 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            using System;
+
+class Program
+        {
+            static void Main()
+            {
+                // Exercise 1
+                int a = 17;
+                int b = 4;
+
+                // Addition
+                int sum = a + b;
+                Console.WriteLine($"Addition: {a} + {b} = {sum}");
+
+                // Subtraction
+                int difference = a - b;
+                Console.WriteLine($"Subtraction: {a} - {b} = {difference}");
+
+                // Multiplication
+                int product = a * b;
+                Console.WriteLine($"Multiplication: {a} * {b} = {product}");
+
+                // Division
+                int quotient = a / b;
+                Console.WriteLine($"Division (Quotient): {a} / {b} = {quotient}");
+
+                // Modulus
+                int remainder = a % b;
+                Console.WriteLine($"Modulus (Remainder): {a} % {b} = {remainder}");
+
+                If a = 17 and b = 4,
+                the quotient is: 4
+                the remainder is: 1
+                
+                //Exercise 2
+
+
+
+            }
+        }
+
+        Console.WriteLine("Hello, World!");
         }
     }
 }
