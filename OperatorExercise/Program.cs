aa487aa480e1295579b5ca8c7e9a828171ae3c8e
tommy santos <tommy1santos@yahoo.com>
2023-10-04 14:21:15 -0400

    done with exercise:

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -6,18 +6,30 @@ namespace OperatorExercise
     {
         static void Main(string[] args)
         {
-            // For division, create interger variables called a and b.
-            var a = 17;
-            var b = 4;
-            var quotient = a / b;
-            var remainder = a % b;
 
+            // Define the variables
+            int a = 17;
+            int b = 4;
 
-            Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}");
+            // Addition
+            int additionResult = a + b;
+            Console.WriteLine($"Addition: {a} + {b} = {additionResult}");
 
-            
+            // Subtraction
+            int subtractionResult = a - b;
+            Console.WriteLine($"Subtraction: {a} - {b} = {subtractionResult}");
 
-           
+            // Multiplication
+            int multiplicationResult = a * b;
+            Console.WriteLine($"Multiplication: {a} * {b} = {multiplicationResult}");
+
+            // Division (casting one operand to double for floating-point division)
+            double divisionResult = (double)a / b;
+            Console.WriteLine($"Division: {a} / {b} = {divisionResult}");
+
+            // Modulus (remainder)
+            int modulusResult = a % b;
+            Console.WriteLine($"Modulus: {a} % {b} = {modulusResult}");
 
             Console.WriteLine(AreaOfCircle());
         }
@@ -29,10 +41,35 @@ namespace OperatorExercise
                 Console.WriteLine("Please enter the radius of your circle:");
                 var radius = double.Parse(Console.ReadLine());
                 var area = Math.PI * Math.Pow(radius, 2);
-            
+
                 return area;
-        
+
+                Console.WriteLine(AreaOfCircle());
             }
-          
-    }
-}
+
+                
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ OperatorExercise/ProgramBase.cs
@@ -0,0 +1,6 @@
+﻿namespace OperatorExercise
+{
+    public class ProgramBase
+    {
+    }
+}
